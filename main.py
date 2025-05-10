afa440d76fbc8455c7baa5e6b32f31848957ce35
galyanrajat <galyanrajat@gmail.com>
2025-05-10 11:56:01 +0530

    add eda,exception,logger,dataset

---
+++ main.py
@@ -0,0 +1,20 @@
+import logging
+from sensor.exception import SensorException
+import sys
+from sensor.logger import logging 
+
+def test_exception():
+    try:
+        logging.info("here the error come and correct this")
+        a=1/0
+    except Exception as e:
+        raise SensorException(e,sys)
+
+
+
+if __name__ == "__main__":
+    try:
+        test_exception()
+    except Exception as e:
+        print(e)
+
